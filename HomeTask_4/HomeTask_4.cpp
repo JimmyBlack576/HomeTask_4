960e8457733ee947f7aa1f523d53ec8d6b1fe0fb
Jimmy Blackwood <Jimmy Blackwood@DESKTOP-AOM0ABL>
2022-05-15 00:47:41 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_4/HomeTask_4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    int sec, min, hours;
+    cout << "Введите секунды: \n";
+    cin >> sec;
+
+    min = sec / 60;
+    sec = sec % 60;
+    hours = min / 60;
+    min = min % 60;
+
+    cout << hours << ":" << min << ":" << sec;
+
+    return 0;
+}
+
